510b4504457388fbdd3919930f643b3528af6e09
zhanghefei <zhanghefei@kuaishou.com>
2022-10-26 11:51:43 +0800

    feat: add isNUll

---
+++ src/functions/isNull.ts
@@ -0,0 +1,6 @@
+export function isNull(str: string | null | undefined ) {
+  if (str === '' || str === null || str === undefined) return true;
+  var reg = '^[ ]+$';
+  var re = new RegExp(reg);
+  return re.test(str);
+}
